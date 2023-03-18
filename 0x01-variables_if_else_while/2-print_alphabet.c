41a5bda854bfb40f014a57488d24268d5edd16aa
Vincidax <vincentdx18@gmail.com>
2023-03-18 02:25:05 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,9 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxwyz";
-	int i;
-
-	for (i = 0; i < 26; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar(alp[i]);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
