b376f7ce13ca081555a39f19b38fd11b618050cc
Vincidax <vincentdx18@gmail.com>
2023-04-11 02:42:17 -0700

    review concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,7 +6,6 @@
  * @s2: input two to concat
  * Return: concat of s1 and s2
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
@@ -16,12 +15,14 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
+
+	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
+
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
@@ -30,12 +31,12 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s1[i];
 		i++;
 	}
+
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
 		i++, ci++;
 	}
 	conct[i] = '\0';
-
 	return (conct);
 }
