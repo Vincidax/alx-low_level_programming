8a9fb8924e80e922d32b8e81b13a6f94e62c0107
Vincent DUSHIME <vincentdx18@gmail.com>
2023-07-10 10:36:33 -0700

    binary to decimal review

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int uns_int = 0;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
@@ -18,9 +18,9 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		uns_int = 2 * uns_int + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (uns_int);
+	return (dec_val);
 }
 
